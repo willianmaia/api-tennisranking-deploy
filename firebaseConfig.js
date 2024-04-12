298afcdfd48e091bf83ba04120634467d3c1157e
Willian Maia <willian.maia@sysmap.com.br>
2024-04-11 22:27:56 -0300

    ajustes

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,25 +1,23 @@
 require('dotenv').config();
 
-console.log('Variáveis de Ambiente:', process.env);
-
-
 const admin = require('firebase-admin');
 
-// Inicialização do Firebase Admin SDK
+const serviceAccount = {
+  type: process.env.TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_X509_CERT_URL
+};
+
 admin.initializeApp({
-  credential: admin.credential.cert({
-    type: process.env.TYPE,
-    project_id: process.env.PROJECT_ID,
-    private_key_id: process.env.PRIVATE_KEY_ID,
-    private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
-    client_email: process.env.CLIENT_EMAIL,
-    client_id: process.env.CLIENT_ID,
-    auth_uri: process.env.AUTH_URI,
-    token_uri: process.env.TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: process.env.CLIENT_X509_CERT_URL
-  }),
-  databaseURL: "https://tennisranking-project-default-rtdb.firebaseio.com"
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: `https://${process.env.PROJECT_ID}.firebaseio.com`
 });
 
-module.exports = admin;
+const db = admin.database(); // Inicialize o acesso ao Realtime Database
