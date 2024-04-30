35f1ba39a66836fa89b1509694c4e3886a32ff83
Willian Maia <willian.maia@sysmap.com.br>
2024-04-30 16:50:05 -0300

    ajuste

--- index.js
+++ index.js
@@ -320,26 +320,43 @@ app.post('/createUser', authenticate, (req, res) => {
     });
 });
 
-
-
 // Rota para login (protegida por autenticação)
 app.post('/login', authenticate, (req, res) => {
   const { email, password } = req.body;
 
-  admin.auth().signInWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-      // Login bem-sucedido
-      res.status(200).send("Login bem-sucedido");
+  // Substituir caracteres inválidos no e-mail para usá-lo como chave
+  const sanitizedEmail = email.replace(/\./g, ',').replace(/@/g, '_');
+
+  // Verificar se o e-mail foi fornecido na solicitação
+  if (!email || !password) {
+    return res.status(400).send("E-mail e senha são obrigatórios");
+  }
+
+  // Verificar se o e-mail e a senha estão corretos
+  admin.database().ref(`/usuarios/${sanitizedEmail}`).once('value')
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        const userData = snapshot.val();
+
+        // Verificar se a senha corresponde à senha armazenada na base de dados
+        if (userData.password === password) {
+          // Senha correta, login bem-sucedido
+          res.status(200).send("Login bem-sucedido");
+        } else {
+          // Senha incorreta
+          res.status(400).send("Credenciais inválidas");
+        }
+      } else {
+        // Usuário não encontrado
+        res.status(400).send("Usuário não encontrado");
+      }
     })
     .catch((error) => {
-      // Tratar erros de login
-      let errorMessage = "Erro ao fazer login";
-      if (error.code === 'auth/user-not-found' || error.code === 'auth/wrong-password') {
-        errorMessage = "Credenciais inválidas";
-      }
-      res.status(400).send(errorMessage);
+      // Tratar erros de consulta ao banco de dados
+      res.status(500).send("Erro ao verificar dados do usuário na base de dados: " + error.message);
     });
 });
+
 
 
 
